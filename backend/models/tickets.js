76746af7cff7d959c2413800b8613352daa2c204
idaloof <99144519+idaloof@users.noreply.github.com>
2023-09-03 01:26:08 +0200

    Updated methods to work with mongodb

--- backend/models/tickets.js
+++ backend/models/tickets.js
@@ -1,39 +1,67 @@
 const database = require('../db/database.js');
 
 const tickets = {
-    getTickets: async function getTickets(req, res){
-        var db = await database.openDb();
+    getTickets: async function getTickets(req, res) {
+        const db = await database.getDb();
+        const allTickets = await db.collection.find({}).toArray();
 
-        var allTickets = await db.all(`SELECT *, ROWID as id FROM tickets ORDER BY ROWID DESC`);
-
-        await db.close();
+        await db.client.close();
 
         return res.json({
             data: allTickets
         });
     },
 
+    // getTickets: async function getTickets(req, res){
+    //     var db = await database.openDb();
+
+    //     var allTickets = await db.all(`SELECT *, ROWID as id FROM tickets ORDER BY ROWID DESC`);
+
+    //     await db.close();
+
+    //     return res.json({
+    //         data: allTickets
+    //     });
+    // },
+
     createTicket: async function createTicket(req, res){
-        var db = await database.openDb();
+        const db = await database.getDb();
 
-        const result = await db.run(
-            'INSERT INTO tickets (code, trainnumber, traindate) VALUES (?, ?, ?)',
-            req.body.code,
-            req.body.trainnumber,
-            req.body.traindate,
-        );
+        const doc = {
+            code: req.body.code,
+            trainnumber: req.body.trainnumber,
+            traindate: req.body.traindate
+        };
 
-        await db.close();
+        const result = await db.collection.insertOne(doc);
 
-        return res.json({
-            data: {
-                id: result.lastID,
-                code: req.body.code,
-                trainnumber: req.body.trainnumber,
-                traindate: req.body.traindate,
-            }
-        });
-    }
+        if (result.ok) {
+            console.log(result.ops);
+            return res.status(201).json({ data: result.ops });
+        }
+    },
+
+    // createTicket: async function createTicket(req, res){
+    //     var db = await database.openDb();
+
+    //     const result = await db.run(
+    //         'INSERT INTO tickets (code, trainnumber, traindate) VALUES (?, ?, ?)',
+    //         req.body.code,
+    //         req.body.trainnumber,
+    //         req.body.traindate,
+    //     );
+
+    //     await db.close();
+
+    //     return res.json({
+    //         data: {
+    //             id: result.lastID,
+    //             code: req.body.code,
+    //             trainnumber: req.body.trainnumber,
+    //             traindate: req.body.traindate,
+    //         }
+    //     });
+    // }
 };
 
 module.exports = tickets;
