81539cbc2bebc7d3d464132f15c9eb3c1c8cbf3a
JuliaLind <116847458+JuliaLind@users.noreply.github.com>
2023-09-02 23:35:50 +0200

    Created vue-project, added scripts for starting frontend- and backend-servers (#9)

--- backend/db/database.js
+++ backend/db/database.js
@@ -17,3 +17,31 @@ const database = {
 };
 
 module.exports = database;
+
+// const mongo = require("mongodb").MongoClient;
+// const config = require("./config.json");
+// const collectionName = "docs";
+
+// const database = {
+//     getDb: async function getDb () {
+//         let dsn = `mongodb://localhost:27017/folinodocs`;
+
+//         if (process.env.NODE_ENV === 'test') {
+//             dsn = "mongodb://localhost:27017/test";
+//         }
+
+//         const client  = await mongo.connect(dsn, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+//         const db = await client.db();
+//         const collection = await db.collection(collectionName);
+
+//         return {
+//             collection: collection,
+//             client: client,
+//         };
+//     }
+// };
+
+// module.exports = database;
