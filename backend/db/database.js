93b8c0c67f47b90def361db00a032decc572a351
idaloof <99144519+idaloof@users.noreply.github.com>
2023-09-03 01:46:43 +0200

    Martin1 (#10)
    * Changed database from sqlite to mongodb
    
    * Updated methods to work with mongodb
    
    * Modified id to _id to match new database
    
    * Installed nodemon as dependency

--- backend/db/database.js
+++ backend/db/database.js
@@ -1,47 +1,49 @@
-const sqlite3 = require('sqlite3').verbose();
-const { open } = require('sqlite');
-
-const database = {
-    openDb: async function openDb() {
-        let dbFilename = `./db/trains.sqlite`;
-
-        if (process.env.NODE_ENV === 'test') {
-            dbFilename = "./db/test.sqlite";
-        }
-
-        return await open({
-            filename: dbFilename,
-            driver: sqlite3.Database
-        });
-    }
-};
-
-module.exports = database;
-
-// const mongo = require("mongodb").MongoClient;
-// const config = require("./config.json");
-// const collectionName = "docs";
+// const sqlite3 = require('sqlite3').verbose();
+// const { open } = require('sqlite');
 
 // const database = {
-//     getDb: async function getDb () {
-//         let dsn = `mongodb://localhost:27017/folinodocs`;
+//     openDb: async function openDb() {
+//         let dbFilename = `./db/trains.sqlite`;
 
 //         if (process.env.NODE_ENV === 'test') {
-//             dsn = "mongodb://localhost:27017/test";
+//             dbFilename = "./db/test.sqlite";
 //         }
 
-//         const client  = await mongo.connect(dsn, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
+//         return await open({
+//             filename: dbFilename,
+//             driver: sqlite3.Database
 //         });
-//         const db = await client.db();
-//         const collection = await db.collection(collectionName);
-
-//         return {
-//             collection: collection,
-//             client: client,
-//         };
 //     }
 // };
 
 // module.exports = database;
+
+const mongo = require("mongodb").MongoClient;
+// const config = require("./config.json");
+const collectionName = "stations";
+
+const database = {
+    getDb: async function getDb () {
+        let dsn = `mongodb://localhost:27017/trains`;
+
+        if (process.env.NODE_ENV === 'test') {
+            dsn = "mongodb://localhost:27017/test";
+        }
+
+        const client  = await mongo.connect(dsn, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        const db = await client.db();
+        const collection = await db.collection(collectionName);
+
+        return {
+            collection: collection,
+            client: client,
+        };
+    }
+};
+
+
+
+module.exports = database;
