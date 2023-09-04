9bbc432f92794a951f2558f343258fbc607e6a70
Julia Lind <juli22@student.bth.se>
2023-09-04 23:05:41 +0200

    Test to move Delayed list component to Vue

---
+++ vue-project-j/src/views/MainView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import DelayedTable from '../components/DelayedTable.vue'
+</script>
+
+<template>
+  <main>
+    <suspense>
+      <DelayedTable />
+    </suspense>
+  </main>
+</template>
+
+<style>
+</style>
