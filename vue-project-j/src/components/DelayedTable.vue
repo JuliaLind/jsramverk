3bac48915b1f9e970b3943fc8c54387abe91e005
JuliaLind <116847458+JuliaLind@users.noreply.github.com>
2023-09-04 23:08:20 +0200

    Julia6 (#12)
    * Updated collections name from 'trains' to 'tickets' in database.js
    
    * Test to move Delayed list component to Vue

---
+++ vue-project-j/src/components/DelayedTable.vue
@@ -0,0 +1,28 @@
+<script setup>
+import DelayItem from './DelayItem.vue'
+
+const res = await fetch("http://localhost:1337/delayed")
+const delayed = await res.json()
+// console.log(delayed);
+</script>
+
+<template>
+  <div class="delayed">
+      <h1>Försenade tåg</h1>
+      <div id="delayed-trains" class="delayed-trains">
+        <DelayItem v-for="(delay, index) in delayed.data" :item="delay" :key="index"/>
+
+      </div>
+  </div>
+</template>
+
+<style scoped>
+.delayed {
+    height: 100vh;
+    width: 40vw;
+    padding: 2rem;
+    overflow: scroll;
+    background-color: white;
+}
+
+</style>
