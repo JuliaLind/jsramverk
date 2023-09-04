c2e3b46e3fb3fb6fbbf2858cc047ce930d422ea4
idaloof <99144519+idaloof@users.noreply.github.com>
2023-09-04 23:08:01 +0200

    Moved frontend code to vue-project directory (#13)

---
+++ vue-project-m/src/components/DelayedTable.vue
@@ -0,0 +1,32 @@
+<!-- Single File Component DelayedTable -->
+
+<!--
+    Script
+ -->
+<script>
+    export default {
+        name: 'DelayedTable',
+        props: {
+            data: Array
+        },
+        methods: {
+            renderTicketView(item) {
+            // Här kommer logiken för renderTicketView-metoden.
+            },
+        },
+    };
+</script>
+
+<!--
+    Template
+-->
+<template>
+    <div>
+        <div v-for="item in data" :key="item.ActivityId" @click="renderTicketView(item)">
+            <DelayedItem :item="item" />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
