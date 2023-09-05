b16bc88648f9846dde353ba9ef71cd4d9efd305f
Julia Lind <juli22@student.bth.se>
2023-09-05 09:51:13 +0200

    map view in place

---
+++ vue-project-j/src/components/MapComp.vue
@@ -0,0 +1,51 @@
+<script>
+const socket = io("http://localhost:1337");
+
+export default {
+ name: "Map",
+ data() {
+    return{
+      center: [62.173276, 14.942265]
+    }},
+    methods: {
+      setupLeafletMap() {
+        const map = L.map('map').setView(this.center, 5);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        let markers = {};
+
+        socket.on("message", (data) => {
+            if (markers.hasOwnProperty(data.trainnumber)) {
+                let marker = markers[data.trainnumber];
+
+                marker.setLatLng(data.position);
+            } else {
+                let marker = L.marker(data.position).bindPopup(data.trainnumber).addTo(map);
+
+                markers[data.trainnumber] = marker;
+            }
+        });
+    },
+ },
+ mounted() {
+    this.setupLeafletMap();
+ },
+};
+
+</script>
+
+
+<template>
+  <div id="map" class="map"></div>
+</template>
+
+<style scoped>
+.map {
+    height: 100vh;
+    width: 60vw;
+}
+</style>
