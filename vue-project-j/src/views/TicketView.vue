da9e7b4fceb7f11328df0dd9f3d2172102eeccb2
Julia Lind <juli22@student.bth.se>
2023-09-05 09:46:40 +0200

    initial draft for the tickets view

---
+++ vue-project-j/src/views/TicketView.vue
@@ -0,0 +1,25 @@
+<script setup>
+    import DelayedTable from '../components/DelayedTable.vue'
+
+  const props = defineProps({
+    item: {
+      type: Array,
+      required: true
+    }
+  })
+
+    const item = props.item;
+    console.log(item);
+
+</script>
+
+<template>
+  <main>
+    <suspense>
+      <DelayedTable />
+    </suspense>
+  </main>
+</template>
+
+<style>
+</style>
