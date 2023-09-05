e1317a102ed2c5c8ec4196346be70fbd51f42b22
Julia Lind <juli22@student.bth.se>
2023-09-05 09:49:02 +0200

    Updated component name from MapView to MapComp

--- vue-project-j/src/views/MainView.vue
+++ vue-project-j/src/views/MainView.vue
@@ -1,14 +1,22 @@
 <script setup>
 import DelayedTable from '../components/DelayedTable.vue'
+import MapComp from '../components/MapComp.vue'
 </script>
 
 <template>
   <main>
-    <suspense>
+  <suspense>
       <DelayedTable />
-    </suspense>
+  </suspense>
+  <MapComp />
   </main>
 </template>
 
 <style>
+
+main {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+}
 </style>
