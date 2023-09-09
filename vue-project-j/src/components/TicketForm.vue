16d9dbbd0d5b50da2aa16b56bf2da99ffdc1dc47
idaloof <99144519+idaloof@users.noreply.github.com>
2023-09-09 10:41:38 +0200

    Data is now being fetched from deployed azure application

--- vue-project-j/src/components/TicketForm.vue
+++ vue-project-j/src/components/TicketForm.vue
@@ -8,7 +8,7 @@ if (item.FromLocation) {
       locationString = `Tåg från ${item.FromLocation[0].LocationName} till ${item.ToLocation[0].LocationName}. Just nu i ${item.LocationSignature}.`;
 }
 
-const res = await fetch("http://localhost:1337/codes");
+const res = await fetch("https://jsramverk-editor-julmar2023.azurewebsites.net/codes");
 const data = await res.json();
 const reasonCodes = await data.data;
 
@@ -25,7 +25,7 @@ async function submitForm() {
         traindate: item.EstimatedTimeAtLocation.substring(0, 10),
     };
 
-    const response = await fetch("http://localhost:1337/tickets", {
+    const response = await fetch("https://jsramverk-editor-julmar2023.azurewebsites.net//tickets", {
           body: JSON.stringify(newTicket),
           headers: {
               'content-type': 'application/json'
