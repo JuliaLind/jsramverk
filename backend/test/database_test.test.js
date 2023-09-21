d4ffbc6c4e0ee86d73535c776561b815a4fc67d6
Julia Lind <juli22@student.bth.se>
2023-09-21 20:23:43 +0200

    commented out test of database model

--- backend/test/database_test.test.js
+++ backend/test/database_test.test.js
@@ -17,19 +17,19 @@ process.env.NODE_ENV = 'test';
 const database = require('../db/database.js');
 // const collectionName = "trains";
 
-describe('database Module test', () => {
-    describe('getDb', () => {
-        it('should connect to the test database and return collection and client references', async () => {
-            // Call the getDb function
+// describe('database Module test', () => {
+//     describe('getDb', () => {
+//         it('should connect to the test database and return collection and client references', async () => {
+//             // Call the getDb function
 
-            const { db, collection, client } = await database.getDb();
+//             const { db, collection, client } = await database.getDb();
 
-            // Assert that collection and client are valid objects
-            expect(collection).to.be.an('object');
-            expect(client).to.be.an('object');
+//             // Assert that collection and client are valid objects
+//             expect(collection).to.be.an('object');
+//             expect(client).to.be.an('object');
 
-            // Close the database connection
-            await client.close();
-        });
-    });
-});
+//             // Close the database connection
+//             await client.close();
+//         });
+//     });
+// });
