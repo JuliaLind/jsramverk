22c5df19c7e85ebb356dcd4cf22f1395fa98ebcc
Julia Lind <juli22@student.bth.se>
2023-09-21 13:54:46 +0200

    givit the extra test one more try, everything is passing locally

--- backend/test/app-tickets.test.js
+++ backend/test/app-tickets.test.js
@@ -15,36 +15,7 @@ const database = require("../db/database.js");
 // const collectionName = "trains";
 
 describe('tickets get and post routes', () => {
-    // beforeEach(() => {
-    //     return new Promise(async (resolve) => {
-    //         const db = await database.getDb();
-
-    //         db.db.listCollections(
-    //             { name: collectionName }
-    //         )
-    //         .next()
-    //         .then(async function(info) {
-    //             await db.collection.drop();
-    //             // if (info) {
-    //             //     await db.collection.drop();
-    //             // }
-    //             const docs = [
-    //                 { code: "ANA002", trainnumber: "9123", traindate: "2023-09-18" },
-    //                 { code: "ANA003", trainnumber: "91234", traindate: "2023-09-18" },
-    //             ];
-    //             await db.collection.insertMany(docs);
-                
-    //         })
-    //         .catch(function(err) {
-    //             console.error(err);
-    //         })
-    //         .finally(async function() {
-    //             await db.client.close();
-    //             resolve();
-    //         });
-    //     });
-    // });
-    beforeEach(async () => {
+    before(async () => {
         const db = await database.getDb();
         await db.collection.drop();
         const docs = [
@@ -98,10 +69,10 @@ describe('tickets get and post routes', () => {
         expect(response.body).to.be.an('object');
         expect(response.body.data.acknowledged).to.equal(true);
 
-        // const id = await JSON.parse(response.text).data.insertedId;
-        // const response2 = await chai.request(server).get('/tickets')
-        // const tickets = await JSON.parse(response2.text).data;
-        // const latest = tickets.filter(ticket => ticket._id === id)[0];
-        // chai.assert.equal(latest.code, 'test_code');
+        const id = await JSON.parse(response.text).data.insertedId;
+        const response2 = await chai.request(server).get('/tickets')
+        const tickets = await JSON.parse(response2.text).data;
+        const latest = tickets.filter(ticket => ticket._id === id)[0];
+        chai.assert.equal(latest.code, 'test_code');
     });
 });
