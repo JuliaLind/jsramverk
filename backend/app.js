f85214e9882ef9d3fd2671b284c2b4d1d2c8826e
Julia Lind <juli22@student.bth.se>
2023-09-30 22:53:36 +0200

    accidentally uncommented the comment, fixed now :)

--- backend/app.js
+++ backend/app.js
@@ -63,7 +63,7 @@ app.use((req, res, next) => {
     next(err);
 });
 
-Error handling middleware
+// Error handling middleware
 app.use((err, req, res, next) => {
     if (res.headersSent) {
         return next(err);
