1ab689de5dbe0fcbda5625f8bda56840196caf56
idaloof <99144519+idaloof@users.noreply.github.com>
2023-10-02 22:35:45 +0200

    M10 (#59)
    * Added cron.schedule to trigger api calls automatically and emit event to vue components in need of delayed trains updates
    
    * Changed from AdvertisedTrainIden to OperationalTrainNumber for consistency through whole application
    
    * Installed node-cron for creating automatic trigger
    
    * Created a delayedTrains ref for reactivity. Component is now using socket to get continuous updates on that ref
    
    * Added a comparison between emitted trainobject and delayed trains array to only display delayed trains on map. An additional socket event is used in this component for delayed trains array.
    
    * Installed bufferutil and utf-8-validate as optional packages

--- backend/models/trains.js
+++ backend/models/trains.js
@@ -26,7 +26,7 @@ const trains = {
             const position = changedPosition.Position.WGS84.match(matchCoords).map((t) => parseFloat(t)).reverse();
 
             const trainObject = {
-                trainnumber: changedPosition.Train.AdvertisedTrainNumber,
+                trainnumber: changedPosition.Train.OperationalTrainNumber,
                 position: position,
                 timestamp: changedPosition.TimeStamp,
                 bearing: changedPosition.Bearing,
@@ -35,12 +35,12 @@ const trains = {
             };
 
             // Emit train position updates to connected clients
-            if (changedPosition.Train.AdvertisedTrainNumber in trainPositions) {
+            if (changedPosition.Train.OperationalTrainNumber in trainPositions) {
                 socket.emit("trainpositions", trainObject);
             }
 
             // Update the train positions object
-            trainPositions[changedPosition.Train.AdvertisedTrainNumber] = trainObject;
+            trainPositions[changedPosition.Train.OperationalTrainNumber] = trainObject;
 
             return trainObject;
             // }
@@ -151,97 +151,5 @@ const trains = {
         }
     }
 };
-
-// /**
-//  * Fetches train positions in real-time and sends updates to
-//  * connected clients via WebSocket.
-//  *
-//  * This function establishes a connection to
-//  * the Trafikverket API using Server-Sent Events (SSE),
-//  * listens for train position updates, and emits those updates
-//  * to connected clients via a WebSocket.
-//  *
-//  * @async
-//  * @function
-//  * @param {Object} io - The Socket.IO instance used for
-//  * WebSocket communication with clients.
-//  *
-//  * @returns {void} No return value but function sends updates to connected clients.
-//  */
-// async function fetchTrainPositions(io) {
-//     // XML Query sent to the API
-//     const query = `<REQUEST>
-//     <LOGIN authenticationkey="${process.env.TRAFIKVERKET_API_KEY}" />
-//     <QUERY sseurl="true" namespace="järnväg.trafikinfo" objecttype="TrainPosition" schemaversion="1.0" limit="1" />
-//     </REQUEST>`;
-
-//     // Object to store train positions
-//     const trainPositions = {};
-
-//     // POST request to Trafikverket API to obtain SSE URL
-//     const response = await fetch(
-//         "https://api.trafikinfo.trafikverket.se/v2/data.json", {
-//             method: "POST",
-//             body: query,
-//             headers: { "Content-Type": "text/xml" }
-//         }
-//     );
-
-//     const result = await response.json();
-//     const sseurl = result.RESPONSE.RESULT[0].INFO.SSEURL;
-
-//     // Create an EventSource to listen for SSE updates
-//     const eventSource = new EventSource(sseurl);
-
-//     eventSource.onopen = function() {
-//         console.log("Connection to server opened.");
-//     };
-
-//     // Handle WebSocket connections from clients
-//     io.on('connection', (socket) => {
-//         console.log('a user connected');
-
-//         // Listen for SSE updates
-//         eventSource.onmessage = function (e) {
-//             try {
-//                 const parsedData = JSON.parse(e.data);
-
-//                 if (parsedData) {
-//                     const changedPosition = parsedData.RESPONSE.RESULT[0].TrainPosition[0];
-
-//                     const matchCoords = /(\d*\.\d+|\d+),?/g;
-
-//                     const position = changedPosition.Position.WGS84.match(matchCoords).map((t=>parseFloat(t))).reverse();
-
-//                     const trainObject = {
-//                         trainnumber: changedPosition.Train.AdvertisedTrainNumber,
-//                         position: position,
-//                         timestamp: changedPosition.TimeStamp,
-//                         bearing: changedPosition.Bearing,
-//                         status: !changedPosition.Deleted,
-//                         speed: changedPosition.Speed,
-//                     };
-
-//                     // Emit train position updates to connected clients
-//                     if (trainPositions.hasOwnProperty(changedPosition.Train.AdvertisedTrainNumber)) {
-//                         socket.emit("message", trainObject);
-//                     }
-
-//                     // Update the train positions object
-//                     trainPositions[changedPosition.Train.AdvertisedTrainNumber] = trainObject;
-//                 }
-//             } catch (e) {
-//                 console.log(e);
-//             }
-
-//             return;
-//         }
-//     });
-
-//     // Handle errors in the EventSource connection
-//     eventSource.onerror = function(e) {
-//         console.log("EventSource failed.");
-//     }
-// }
 
 module.exports = trains;
