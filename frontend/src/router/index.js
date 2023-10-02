28d88767d7ac8c6e17f4d278408409a78f097d22
Julia Lind <juli22@student.bth.se>
2023-10-02 19:01:21 +0200

    changed name of tickets route to admin, added a beforeEach hook for regirecting o login page if no token

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,24 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainView from '../views/MainView.vue'
-import TicketView from '../views/TicketView.vue'
+import AdminView from '../views/AdminView.vue'
+import LoginView from '../views/LoginView.vue'
 
-const routes = [
+import { useAuthStore } from '@/stores/auth';
+
+
+export const routes = [
     {
         path: '/',
         name: 'main',
         component: MainView
     },
     {
-        path: '/tickets',
-        name: 'tickets',
-        component: TicketView
-    }
+        path: '/admin',
+        name: 'admin',
+        component: AdminView
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: LoginView
+    },
 ]
 
-const router = createRouter({
+export const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: routes
 })
 
-export { routes }
-export default router
+router.beforeEach(async (to) => {
+    // redirect to login page if not logged in and trying to access a restricted page
+    const publicPages = ['/login', '/', '/register'];
+    const authRequired = !publicPages.includes(to.path);
+    const auth = useAuthStore();
+
+    if (authRequired && !auth.getToken()) {
+        auth.returnUrl = to.fullPath;
+        return '/login';
+    }
+});
+
+// export { routes }
+// export default router
