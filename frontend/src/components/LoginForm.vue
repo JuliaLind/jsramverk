7b4981e480c52d3e945b9c1398ed2b6d343cde4b
Julia Lind <juli22@student.bth.se>
2023-10-02 19:02:36 +0200

    login form now functioning

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -14,11 +14,12 @@ let password = "";
         <RouterLink to="/">Tillbaka</RouterLink>
         <h1>Logga in</h1>
         <form v-on:submit.prevent="store.login(username, password)">
+            <label>E-postaddress</label>
             <input type="email" name="username" required="required" v-model="username"/><br />
+            <label>Lösenord</label>
             <input type="password" required="required" name="password" v-model="password" /><br />
             <input type="submit" value="Logga in" />
         </form>
-        <!-- <RouterLink to="/register">Registrera</RouterLink> -->
     </div>
 </template>
 
