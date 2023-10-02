99c3ab4a357f7eaf0eb96e7b2fa280bf0b55d5ab
idaloof <99144519+idaloof@users.noreply.github.com>
2023-10-02 21:52:02 +0200

    Created a delayedTrains ref for reactivity. Component is now using socket to get continuous updates on that ref

--- frontend/src/components/DelayedTable.vue
+++ frontend/src/components/DelayedTable.vue
@@ -4,15 +4,29 @@
  */
 import { getDelayedTrains } from '../services/api.service.js'
 import DelayedItem from './DelayedItem.vue'
+import { onMounted, ref } from 'vue'
+import { io } from 'socket.io-client';
 
-const data = await getDelayedTrains()
+const delayedTrains = ref([])
+
+onMounted(async () => {
+    delayedTrains.value = await getDelayedTrains()
+})
+
+const socket = io(import.meta.env.VITE_URL)
+
+socket.on('delayedTrainsUpdate', (updatedTrains) => {
+    delayedTrains.value = updatedTrains
+})
+
+// const data = await getDelayedTrains()
 </script>
 
 <template>
     <div class="delayed">
         <h1>Försenade tåg</h1>
         <div id="delayed-trains" class="delayed-trains">
-            <DelayedItem v-for="(item, index) in data" :item="item" :key="index" />
+            <DelayedItem v-for="item in delayedTrains" :item="item" :key="item.ActivityId" />
         </div>
     </div>
 </template>
