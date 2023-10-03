04db34c2ccb77287e03ec19ff62f01f1bdffafe5
Julia Lind <juli22@student.bth.se>
2023-10-03 12:46:27 +0200

    started on some code for keeping delayed list central and updated cenreally in store for all components to use, but is not working as planned so keeping it commented out for now

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,19 @@
 <script setup>
-// import { RouterLink, RouterView } from 'vue-router'
 import { RouterView } from 'vue-router'
+
+// Work in Progress - not working yet
+// import socket from './services/socket.service.js'
+// import { useTrainsStore } from './stores/trains.js'
+// import { getDelayedTrains } from './services/api.service.js'
+// import { onMounted } from 'vue'
+// const store = useTrainsStore()
+// onMounted(async () => {
+//     store.setTrains(await getDelayedTrains())
+//     console.log("direct value on mount", store.delayed.value);
+// });
+// socket.on('delayedTrainsUpdate', (updatedTrains) => {
+//     store.setTrains(updatedTrains)
+// })
 </script>
 
 <template>
