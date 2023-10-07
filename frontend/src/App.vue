7ca88ca50c510c8ad603b70828b28be4a95f6d11
Julia Lind <juli22@student.bth.se>
2023-10-07 20:46:48 +0200

    added some lines to request reasoncodes from app and stor in trainsstore to be re-used in the ticket select options

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,20 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import { getCodes, getDelayedTrains } from './services/api.service.js'
+import socket from './services/socket.service.js'
+import { useTrainsStore } from './stores/trains.js'
+import { onMounted } from 'vue'
+const store = useTrainsStore()
 
-// Work in Progress - not working yet
-// import socket from './services/socket.service.js'
-// import { useTrainsStore } from './stores/trains.js'
-// import { getDelayedTrains } from './services/api.service.js'
-// import { onMounted } from 'vue'
-// const store = useTrainsStore()
-// onMounted(async () => {
-//     store.setTrains(await getDelayedTrains())
-//     console.log("direct value on mount", store.delayed.value);
-// });
-// socket.on('delayedTrainsUpdate', (updatedTrains) => {
-//     store.setTrains(updatedTrains)
-// })
+// store data to be used in admin view
+onMounted(async () => {
+    store.setDelayed(await getDelayedTrains())
+    store.setCodes(await getCodes())
+});
+
+socket.on('delayedTrainsUpdate', (updatedTrains) => {
+    store.setDelayed(updatedTrains)
+})
 </script>
 
 <template>
