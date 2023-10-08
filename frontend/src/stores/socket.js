0d8184425fc5e412a17eba229fd176e84b85b6e9
idaloof <99144519+idaloof@users.noreply.github.com>
2023-10-08 20:31:28 +0200

    Added socket store to handle events emitted to and from backend

---
+++ frontend/src/stores/socket.js
@@ -0,0 +1,24 @@
+import { defineStore } from 'pinia'
+// import { ref } from 'vue'
+import socket from '../services/socket.service.js'
+
+
+export const socketStore = defineStore('socket', {
+    state: () => ({ data: null }),
+    actions: {
+        notifyBackendTicketEdit(data) {
+            socket.emit('edit-ticket', data)
+        },
+        receiveFromBackendTicketEdit() {
+            socket.on('editing-ticket', (data) => {
+                this.data = data;
+            });
+        }
+    },
+    getters: {
+        getDataFromBackend() {
+            return this.data
+        }
+    }
+})
+
