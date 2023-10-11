a0043691f07771b2477540d680ea28deb05d34ed
Julia Lind <juli22@student.bth.se>
2023-10-11 03:11:51 +0200

    frontend/src/components/RegisterForm.vue

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -1,10 +1,25 @@
 <script setup>
 import { useAuthStore } from '@/stores/auth'
 import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
 
 const store = useAuthStore()
 let username = ''
 let password = ''
+
+let innerText = ref("visibility")
+let fieldType = ref("password")
+
+function toggleVisibility() {
+    if (innerText.value === "visibility" && fieldType.value === "password") {
+        innerText.value = "visibility_off"
+        fieldType.value = "text"
+    } else {
+        innerText.value = "visibility"
+        fieldType.value = "password"
+    }
+}
+
 </script>
 
 <template>
@@ -15,10 +30,17 @@ let password = ''
             <label>E-postaddress</label>
             <input type="email" name="username" required="required" v-model="username" /><br />
             <label>Lösenord</label>
-            <input type="password" required="required" name="password" v-model="password" /><br />
+            <input v-bind:type="fieldType" required="required" name="password" v-model="password" />
+            <span class="material-symbols-outlined" @click="toggleVisibility()">
+{{ innerText }}
+</span>
+<br />
             <input type="submit" value="Logga in" />
         </form>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.material-symbols-outlined {
+    cursor: pointer;
+}</style>
