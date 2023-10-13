77578a203ecae24903ef39c5dd11d4c113ef6ea2
idaloof <99144519+idaloof@users.noreply.github.com>
2023-10-14 01:45:31 +0200

    Changed structure of template to fit bootstrap

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -22,59 +22,43 @@ function toggleVisibility() {
 </script>
 
 <template>
-<div>
-    <div class="container mt-5">
-        <div class="row justify-content-center">
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-header h5">Logga In</div>
-                    <div class="card-body">
-                        <form v-on:submit.prevent="store.login(username, password)">
-                            <div class="form-group mb-3">
-                                <label class="mb-2" for="userName">E-postadress</label>
-                                <input class="form-control" type="email" name="username" required="required" v-model="username" />
-                            </div>
-                            <div class="form-group mb-3">
-                                <label class="mb-2" for="password">Lösenord</label>
-                                <div class="password-container">
-                                    <input
-                                        class="form-control"
-                                        v-bind:type="fieldType"
-                                        required="required"
-                                        name="password"
-                                        v-model="password"
-                                    />
-                                    <span class="material-symbols-outlined" @click="toggleVisibility()">
-                                        {{ innerText }}
-                                    </span>
+    <div>
+        <div class="container mt-5">
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header h5">Logga in</div>
+                        <div class="card-body">
+                            <form v-on:submit.prevent="store.login(username, password)">
+                                <div class="form-group mb-3">
+                                    <label class="mb-2" for="userName">E-postadress</label>
+                                    <input class="form-control" type="email" name="username" required="required" v-model="username" />
                                 </div>
-                            </div>
-                            <div class="form-group">
-                                <input type="submit" value="Logga in" class="btn btn-dark mt-3" />
-                            </div>
-                        </form>
+                                <div class="form-group mb-3">
+                                    <label class="mb-2" for="password">Lösenord</label>
+                                    <div class="password-container">
+                                        <input
+                                            class="form-control"
+                                            v-bind:type="fieldType"
+                                            required="required"
+                                            name="password"
+                                            v-model="password"
+                                        />
+                                        <span class="material-symbols-outlined" @click="toggleVisibility()">
+                                            {{ innerText }}
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="submit" value="Logga in" class="btn btn-dark mt-3" />
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-
-    <!--<div class="ticket">
-        <RouterLink to="/">Tillbaka</RouterLink>
-        <h1>Logga in</h1>
-        <form v-on:submit.prevent="store.login(username, password)">
-            <label>E-postaddress</label>
-            <input type="email" name="username" required="required" v-model="username" /><br />
-            <label>Lösenord</label>
-            <input v-bind:type="fieldType" required="required" name="password" v-model="password" />
-            <span class="material-symbols-outlined" @click="toggleVisibility()">
-                {{ innerText }}
-            </span>
-            <br />
-            <input type="submit" value="Logga in" />
-        </form>
-    </div>-->
 </template>
 
 <style scoped>
