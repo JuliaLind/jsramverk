6653e431a6e661929df1cf3c7afc9628af8efa54
idaloof <99144519+idaloof@users.noreply.github.com>
2023-10-14 01:48:41 +0200

    Added styling to logout button and changed template structure

--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import TicketTable from '../components/TicketTable.vue'
-import Header from '../components/Header.vue';
-import Footer from '../components/Footer.vue';
+import HeaderComp from '../components/HeaderComp.vue';
+import FooterComp from '../components/FooterComp.vue';
 import { RouterLink } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 const store = useAuthStore()
@@ -9,26 +9,32 @@ const store = useAuthStore()
 
 <template>
     <div class="main-nav">
-        <Header></Header>
+        <HeaderComp></HeaderComp>
+    </div>
+    <div class="logout">
+        <RouterLink class="btn btn-warning" to="/" @click="store.logout()"> Logga ut </RouterLink>
     </div>
     <div class="container">
         <!-- Note for later: this one does not need to be a router link
         component, it is possible to do a router.push to for example /login from auth store -->
-        <RouterLink to="/" @click="store.logout()"> Logga ut </RouterLink>
-        <br />
-        <RouterLink to="/"> Tillbaka </RouterLink>
         <div class="ticket-container">
             <TicketTable />
         </div>
     </div>
-    <div class="footer fixed-bottom">
-        <Footer></Footer>
+    <div class="navbar navbar-fixed-bottom">
+        <FooterComp></FooterComp>
     </div>
 
 </template>
 
 <style>
-.ticket-container {
-    padding: 2rem;
+.logout {
+    text-align: end;
+    margin: 1.5em 1.5em;
 }
+
+.ticket-container {
+    padding: 0;
+}
+
 </style>
