991a8af600a4450ee81b4993a50b2809aa9920d5
idaloof <99144519+idaloof@users.noreply.github.com>
2023-10-13 21:14:23 +0200

    Added header and footer

--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -1,19 +1,30 @@
 <script setup>
 import TicketTable from '../components/TicketTable.vue'
+import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
 import { RouterLink } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 const store = useAuthStore()
 </script>
 
 <template>
-    <!-- Note for later: this one does not need to be a router link
-    component, it is possible to do a router.push to for example /login from auth store -->
-    <RouterLink to="/" @click="store.logout()"> Logga ut </RouterLink>
-    <br />
-    <RouterLink to="/"> Tillbaka </RouterLink>
-    <div class="ticket-container">
-        <TicketTable />
+    <div class="main-nav">
+        <Header></Header>
     </div>
+    <div class="container">
+        <!-- Note for later: this one does not need to be a router link
+        component, it is possible to do a router.push to for example /login from auth store -->
+        <RouterLink to="/" @click="store.logout()"> Logga ut </RouterLink>
+        <br />
+        <RouterLink to="/"> Tillbaka </RouterLink>
+        <div class="ticket-container">
+            <TicketTable />
+        </div>
+    </div>
+    <div class="footer fixed-bottom">
+        <Footer></Footer>
+    </div>
+
 </template>
 
 <style>
