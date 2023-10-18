b5999f258d23ac3fcc1e601239ce854ff130ebfb
Julia Lind <juli22@student.bth.se>
2023-10-18 16:45:38 +0200

    added test for toggling between showing all trains and one by clicking on an item

--- frontend/src/components/__tests__/TicketTable.spec.js
+++ frontend/src/components/__tests__/TicketTable.spec.js
@@ -1,53 +1,14 @@
-import { vi, describe, it, expect, afterEach } from 'vitest'
+import { vi, describe, it, expect, afterEach, beforeEach } from 'vitest'
 import TicketTable from '../TicketTable.vue'
 import { mount, flushPromises } from '@vue/test-utils'
 import { tickets } from './mockdata/tickets.js'
 import { codes } from './mockdata/codes-small.js'
 import { trainnumbers } from './mockdata/trainnumbers.js'
-
-vi.mock('@/stores/auth', () => ({
-    useAuthStore: () => ({
-        token: 'imavalidtoken',
-        getToken: vi.fn(() => {
-            return 'imavalidtoken'
-        }),
-        updateTicket: () => {
-            return 'ok'
-        },
-        deleteTicket: () => {
-            return 'ok'
-        },
-        getTickets: vi.fn(() => {
-            return tickets
-        })
-    })
-}))
-
-vi.mock('@/stores/socket', () => ({
-    socketStore: () => ({
-        data: {
-            '6505d0b1a60773cde6d0704d': 'user@email.com'
-        },
-        notifyBackendEdit(data) {
-            console.log(data)
-        },
-        notifyBackendStopEdit(data) {
-            console.log(data)
-        },
-        listenForTicketLock() {
-            //do nothing
-        },
-        listenForTicketUnlock() {
-            //do nothing
-        }
-    })
-}))
+import { setActivePinia, createPinia } from 'pinia'
+import { useAuthStore } from '@/stores/auth'
 
 vi.mock('../../services/api.service.js', () => {
     return {
-        getCodes: vi.fn(() => {
-            return codes
-        }),
         getTrainNumbers: vi.fn(() => {
             return trainnumbers
         })
@@ -58,17 +19,25 @@ describe('TicketsTable', async () => {
     afterEach(() => {
         vi.restoreAllMocks()
     })
+    beforeEach(() => {
+        setActivePinia(createPinia())
+    })
 
     it('renders properly', async () => {
+        const store = useAuthStore()
+        store.getTickets = vi.fn(() => {
+            return tickets
+        })
+        store.codes = codes;
         const wrapper = mount(TicketTable)
         await flushPromises()
         expect(wrapper.text()).contains('Befintliga ärenden')
         expect(wrapper.text()).contains('Ärendenummer')
-        // expect(wrapper.text()).toContain('Skapa')
-        expect(wrapper.text()).contains('Ändra')
+        expect(wrapper.html()).toContain('Skapa')
+        expect(wrapper.html()).toContain('Ändra')
         expect(wrapper.text()).contains('Ta bort')
-        // expect(wrapper.text()).contains('6505c49ab3546c7d65e58f89')
-        // expect(wrapper.text()).contains('65071ede53fecc7e2c2c1732')
+        expect(wrapper.html()).contains('6505c49ab3546c7d65e58f89')
+        expect(wrapper.html()).contains('65071ede53fecc7e2c2c1732')
         wrapper.unmount()
     })
 })
