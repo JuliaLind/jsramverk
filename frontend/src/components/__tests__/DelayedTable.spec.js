95876b5634452cc449fe3c7c690333961ec45492
Julia Lind <juli22@student.bth.se>
2023-10-18 21:52:05 +0200

    added some comments

--- frontend/src/components/__tests__/DelayedTable.spec.js
+++ frontend/src/components/__tests__/DelayedTable.spec.js
@@ -42,11 +42,14 @@ describe('DelayedTable', async () => {
         expect(wrapper.html()).toContain('20096')
         expect(store.current).toBe('')
 
+        // click and now the delayed table should only show delays from train 8136
         await wrapper.find('.delay-item').trigger('click')
         expect(store.current).toBe('8136')
         expect(wrapper.html()).toContain('8136')
         expect(wrapper.html()).not.toContain('8739')
         expect(wrapper.html()).not.toContain('20096')
+
+        // click and now the delayed table should only show delays from all trains
         await wrapper.find('.delay-item').trigger('click')
         expect(store.current).toBe('')
         expect(wrapper.html()).toContain('8739')
