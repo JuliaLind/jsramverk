0f809db31bb3fe39c7a585d0856408db262e316e
Julia Lind <juli22@student.bth.se>
2023-10-19 21:57:50 +0200

    register and login forms now have full coverage

--- frontend/src/components/__tests__/LoginForm.spec.js
+++ frontend/src/components/__tests__/LoginForm.spec.js
@@ -1,6 +1,6 @@
 import { vi, describe, it, expect, afterEach, beforeEach } from 'vitest'
 import LoginForm from '../LoginForm.vue'
-import { mount, flushPromises } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import { setActivePinia, createPinia } from 'pinia'
 import { useAuthStore } from '@/stores/auth'
 
@@ -17,7 +17,6 @@ describe('LoginForm', async () => {
         auth.login = vi.fn()
         const wrapper = mount(LoginForm)
 
-        await flushPromises()
         expect(wrapper.text()).contains('Logga in')
         expect(wrapper.text()).contains('E-postadress')
         expect(wrapper.text()).contains('Lösenord')
@@ -29,20 +28,23 @@ describe('LoginForm', async () => {
 
         wrapper.unmount()
     })
-    it('tests to toggle visibility of password button', async () => {
+    it('tests to toggle visibility of password by toggling the visibility button', async () => {
         const wrapper = mount(LoginForm)
-
-        await flushPromises()
         let password = wrapper.find('input[name=password]')
         expect(password.html()).toContain('type="password"')
         expect(password.html()).not.toContain('type="text"')
         let btn = wrapper.find('.material-symbols-outlined')
-        expect(btn.text()).toBe("visibility")
+        expect(btn.text()).toBe('visibility')
         await btn.trigger('click')
-        expect(btn.text()).toBe("visibility_off")
+        expect(btn.text()).toBe('visibility_off')
         password = wrapper.find('input[name=password]')
         expect(password.html()).not.toContain('type="password"')
         expect(password.html()).toContain('type="text"')
+        await btn.trigger('click')
+        expect(btn.text()).toBe('visibility')
+        password = wrapper.find('input[name=password]')
+        expect(password.html()).toContain('type="password"')
+        expect(password.html()).not.toContain('type="text"')
         wrapper.unmount()
     })
 })
